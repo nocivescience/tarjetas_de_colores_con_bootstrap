33ecaca97678e5d0818059ed84ff913bfe15e323
noceive <nocivescience@gmail.com>
2022-05-13 22:20:15 -0400

    proyecto con la base ya incluida

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 const express=require('express');
 const path=require('path');
-const {mongodb}=require('');
+const {mongoose}=require('./database');
+const app=express();
+const morgan=require('morgan');
+//settings
+app.set('port',process.env.PORT||3000);
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+//routes
+app.use('/api/notes',require('./routes/note.routes'));
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+//starting the server
+app.listen(app.get('port'),()=>{
+    console.log('Server on port',app.get('port'));
+});
