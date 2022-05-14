d3724b8c9a7da9ae0f44d3802a4ce22845675754
noceive <nocivescience@gmail.com>
2022-05-14 02:20:04 -0400

    Creando un nuevo comienzo

--- src/client/practica.js
+++
@@ -1,2 +0,0 @@
-const word='hola';
-console.log(JSON.parse(word));
--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require('mongoose');
-const URI='mongodb://localhost/notesdb';
-mongoose.connect(URI)
-    .then(db=>console.log('DB is connected'))
-    .catch(err=>console.log(err));
